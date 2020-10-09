e3f5ef5f0b9f37472b76e966afbad50bfc086c43
Ashley Zink <pamperedzink@gmail.com>
2020-10-09 16:29:08 -0600

    initial commit

---
+++ src/com/skilldistillery/filmquery/entities/Actor.java
@@ -0,0 +1,49 @@
+package com.skilldistillery.filmquery.entities;
+
+import java.util.List;
+
+public class Actor {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private List<Film> films;
+
+	@Override
+	public String toString() {
+		return "Actor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", films=" + films + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Film> getFilms() {
+		return films;
+	}
+
+	public void setFilms(List<Film> films) {
+		this.films = films;
+	}
+
+}
